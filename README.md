9a4db5a316b459a67be9489aecbb3681c33e9450
Gabriel Santos <gabrielprogramado@gmail.com>
2023-08-05 00:58:35 -0300

    Init Mod

---
+++ README.md
@@ -0,0 +1,10 @@
+# Worm World - Connect
+
+## [Worm World](https://wormworld.io)
+
+Worm World -  Connecting teams and friends
+
+## Contato
+
+contato@wormworld.io
+# wormworld-mod
---
+++ background.js
@@ -0,0 +1,5 @@
+const wormworld_extension = true;
+console.log(
+  "%c WormWorld - Connect (Nº1) Cracked by Gabriel",
+  "padding: 5px;font-size: 14px; color: white; background-color: red;"
+);
